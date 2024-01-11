bf707e5eac880638238f7a2137fe0513b9003075
ThePaulin <kubwirapaulin@gmail.com>
2024-01-10 23:32:53 -0500

    add loop, handle err on string input

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,43 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
 
     println!("Guess the number");
 
-    println!("Please input your guess.");
+    //generating secrete number
+    let secrete_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+            println!("Please input your guess.");
     
     //create a mutable variable to store user input  
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    
-    println!("You guessed: {guess} \n");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    //create shodow for line (reassign)
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, //prevent code from breaking on wrong input
+        };
+        println!("You guessed: {guess} \n the secrete number was {secrete_number}");
 
-    let x: i16 = 5;
-    let y: i16 = 10;
+    //validating guess
+        match guess.cmp(&secrete_number) {
+            Ordering::Less => println!("Too samll!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!!");
+                break;
+            },
+        }
+    } 
+    //let x: i16 = 5;
+    //let y: i16 = 10;
 
-    println!("x = {x} and y + 2 = {}", y + 2);
-    //generating secrete number
+    //println!("x = {x} and y + 2 = {}", y + 2);
 
 
 }
